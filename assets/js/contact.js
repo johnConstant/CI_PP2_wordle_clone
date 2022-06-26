614ed484d8e5576aa0118d70755f77a0e3f4ebad
John Constant <johnconstantis@gmail.com>
2022-06-26 12:30:18 +0000

    Add emailJS code to contact.js

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,1 +1,19 @@
-alert('connected!');
+(function() {
+    // https://dashboard.emailjs.com/admin/account
+    emailjs.init('-BeZauXTcWVY9NBqG');
+})();
+
+window.onload = function() {
+    document.getElementById('contact-form').addEventListener('submit', function(event) {
+        event.preventDefault();
+        // generate a five digit number for the contact_number variable
+        this.contact_number.value = Math.random() * 100000 | 0;
+        // these IDs from the previous steps
+        emailjs.send("wordle_clone","wordle_clone_template", this)
+            .then(function() {
+                console.log('SUCCESS!');
+            }, function(error) {
+                console.log('FAILED...', error);
+            });
+    });
+}
